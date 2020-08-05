8813624b1089d4762045492fdccb29f6616496c2
Kanawat Phuengphadung <kanawatphu@mathregit.group>
2020-08-05 13:57:38 +0700

    add page and itemPerPage to display on browser

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -20,17 +20,25 @@ public class UserController {
 
     @GetMapping("/users")   //GET is HTTP GET method.
     @ResponseBody
-    public List<UsersResponse> getAllUser(@RequestParam(defaultValue = "1") int page, @RequestParam(defaultValue = "10") int item_per_page){
+    //public List<UsersResponse> getAllUser(
+    public PagingResponse getAllUser(
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(name = "inter_per_page", defaultValue = "10") int itemPerPage) { //name is on the URL variable
 
         //monitoring
         System.out.println("page value: " + page);
-        System.out.println("item_per_page value: " + item_per_page);
+        System.out.println("item_per_page value: " + itemPerPage);
+
+        PagingResponse pagingResponse = new PagingResponse(page, itemPerPage);
 
         List<UsersResponse> usersResponsesList = new ArrayList<>();
         usersResponsesList.add(new UsersResponse(1, "User 1"));
         usersResponsesList.add(new UsersResponse(2, "User 2"));
         usersResponsesList.add(new UsersResponse(3, "User 3"));
-        return usersResponsesList;
+
+        pagingResponse.setUsersResponse(usersResponsesList);
+
+        return pagingResponse;
     }
 
     @GetMapping("/users/{id}") // { } is path variable --> get this value from typing url on browser.
