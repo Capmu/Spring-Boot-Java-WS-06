a336e29133345e022566c146a5b9d4dfe3c425e6
Kanawat Phuengphadung <kanawatphu@mathregit.group>
2020-08-05 11:07:34 +0700

    step_01

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -9,8 +9,8 @@ public class UserController {
     @GetMapping("/users")   //GET is HTTP GET method.
     public UsersResponse[] getAllUser(){    //add [ ] because we wanna return array.
         UsersResponse[] usersResponses = new UsersResponse[2];
-        usersResponses[0] = new UsersResponse();
-        usersResponses[1] = new UsersResponse();
+        usersResponses[0] = new UsersResponse(1, "User 1");
+        usersResponses[1] = new UsersResponse(2, "User 2");
         return usersResponses;
     }
 }
