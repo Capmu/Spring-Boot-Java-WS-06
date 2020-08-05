1a1fa22864eaa202da7ca2b5fcedd91f911ac07c
Kanawat Phuengphadung <kanawatphu@mathregit.group>
2020-08-05 10:58:29 +0700

    doing step2

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -7,7 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @GetMapping("/users")   //GET is HTTP GET method.
-    public String getAllUser(){
-        return "Get All User, Hello world !";
+    public UsersResponse[] getAllUser(){    //add [ ] because we wanna return array.
+        UsersResponse[] usersResponses = new UsersResponse[2];
+        usersResponses[0] = new UsersResponse();
+        usersResponses[1] = new UsersResponse();
+        return usersResponses;
     }
 }
---
+++ src/main/java/com/example/demo/UsersResponse.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+public class UsersResponse {
+    private int id;
+    private String name;
+
+    //POJO (Plain Old Java Object)
+    //Getter and Setter methods
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
