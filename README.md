003d683586d7dbd93dbe0e7fead48a8b3e3377bd
Kanawat Phuengphadung <kanawatphu@mathregit.group>
2020-08-05 10:46:00 +0700

    STEP1 done.

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
-## Building Command Steps
+## Workshop Spring Boot 101
+
+####Run in development mode
 1. open terminal.
 2. ```$gradlew```
 3. ```$gradlew test```
 4. ```$gradlew bootRun```
+
+####Run in development mode
+1. ```$gradlew bootJar```
+2. ```$java -jar build\libs\demo-0.0.1-SNAPSHOT.jar```
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,9 @@
-package com.example.demo;
+package com.example.demo; //this is fundamental package, the next package that we want to add must exist in this package
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication //can have only once and this file must have main.
 public class DemoApplication {
 
 	public static void main(String[] args) {
